3920310082d16e3a7406008c10bed954b90fd0a3
Kamil Łepek <kamil.lepek94@gmail.com>
2018-08-21 22:30:30 +0000

    Refactor and added help

--- Remote.py
+++ Remote.py
@@ -9,50 +9,62 @@ Ab = AlphaBot()
 IR = 18
 PWM = 50
 
+def printHelp():
+        file = open('help.txt', 'r')
+        print file.read()
+        file.close()
+        return
+
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 GPIO.setup(IR,GPIO.IN,GPIO.PUD_UP)
 try:
 	input = sys.argv[1]
+	availableCommands = ["forward", "backward", "left", "right"]
+	if input not in availableCommands:
+		printHelp()
+		exit()
 	try:
 		duration = int(sys.argv[2])
 	except ValueError:
-		print("Failed to parse second argument")
+		print("Failed to parse duration argument")
 		GPIO.cleanup()
 		exit()
+	except IndexError:
+		print("Please enter duration [ms]")
+		print("python Remote.py <COMMAND> <DURATION> [<PWM>]")
+		exit()
+	try:
+		PWM = int(sys.argv[3])
+		if(PWM>100):
+			PWM = 100
+			print("Maximum PWM is 100")
+		if(PWM<35):
+			PWM=35
+			print("Minimum PWM is 35")
+		Ab.setPWMA(PWM)
+		Ab.setPWMB(PWM)
+	except ValueError:
+		print("Failed to parse PWM argument")
+		GPIO.cleanup()
+		exit()
+	except IndexError:
+		pass
 	while (duration>0):
 		if(input != None):
-			if input == "cleanup":
-				GPIO.cleanup()
 			if input == "forward":
 				Ab.forward()
-				print("forward")
 			if input == "left":
 				Ab.left()
-				print("left")
-			if input == "stop":
-				Ab.stop()
-				print("stop")
 			if input == "right":
 				Ab.right()
-				print("right")
 			if input == "backward":
 				Ab.backward()
-				print("backward")
-			if input == "faster":
-				if(PWM + 10 < 101):
-					PWM = PWM + 10
-					Ab.setPWMA(PWM)
-					Ab.setPWMB(PWM)
-					print(PWM)
-			if input == "slower":
-				if(PWM - 10 > -1):
-					PWM = PWM - 10
-					Ab.setPWMA(PWM)
-					Ab.setPWMB(PWM)
-					print(PWM)
 		duration=duration-1
 		time.sleep(0.001)
 except KeyboardInterrupt:
 	GPIO.cleanup()
+except IndexError:
+	printHelp()
+        exit()
 GPIO.cleanup()
